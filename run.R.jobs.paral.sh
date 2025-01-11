11b0035e3c0d09a38d2760a301437ea414ea323d
Guillermo Martinez-Boggio <102411099+GMBog@users.noreply.github.com>
2025-01-11 16:17:49 -0600

    Update run.R.jobs.paral.sh

--- run.R.jobs.paral.sh
+++ run.R.jobs.paral.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
-#SBATCH --job-name=Crossval_GM
+#SBATCH --job-name=Crossval
 #SBATCH --mail-user=@gmail.com
 #SBATCH --mail-type=END,FAIL,ARRAY_TASKS
 #SBATCH --ntasks=1 
 #SBATCH --array=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=10mb                     #memory per each job
-#SBATCH --time=90:00:00                                 #time per each job
-#SBATCH --output=PRED_GM_crossval.out                  #Output and error log or -o (all error goes here if --error not especified)
-#SBATCH --error=PRED_GM_crossval.error                 #Error log or e
+#SBATCH --time=90:00:00                        #time per each job
+#SBATCH --output=PRED_crossval.out             #Output and error log or -o (all error goes here if --error not especified)
+#SBATCH --error=PRED_crossval.error            #Error log or e
 
+# List of numbers to use as SEED to execute a random process in R
+SEED=(123 1001 1010 1100 2001)  # 5 replicates
+# List of the number of folds
+FOLD=(1 2 3 4 5 6 7 8 9 10)     # 10-fold cross-validation
 
-#SEED=(123 1001 1010 1100 2001 101 313 3002 231 4440)           #10 replicates
-SEED=(123 1001 1010 1100 2001)                                  #5 replicates   
-FOLD=(1 2 3 4 5 6 7 8 9 10)                                     #n folds crossvalidation
-
-
-### Loop to do CV using the whole microbial matrix
-
-for t in {1..3}
+# Loop to run CV for phenotype predictions of 3 traits
+for t in {1..3} # Number of traits
 do
-        for i in "${SEED[@]}"
+    for i in "${SEED[@]}" # Loop over replicates list
+    do
+        for j in "${FOLD[@]}" # Loop over folds list
         do
-                for j in "${FOLD[@]}"
-                do
-
-                        sbatch R.jobs.paral.sh $t $i $j
-                        sleep 1                                         #wait 1 secod to submit next job
-                done
+            # Submit a job with the 3 arguments (trait, seed, fold)
+            sbatch R.jobs.paral.sh $t $i $j
+            sleep 1 # Wait 1 second to submit the next job
         done
+    done
 done
+
